3f58efe768ceed2a8f17ebf29a3d1fc1c7f6e22d
Duda Elbakidze <147228294+duda-el@users.noreply.github.com>
2023-11-19 17:27:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navBar
 const menuOpen = document.querySelector(".men");
 const menuClose = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
@@ -9,3 +10,38 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+
+//signIn
+const userName = document.getElementById("userName");
+const password = document.querySelector("#password");
+const button = document.querySelector("#btn");
+const usernameError = document.getElementById("usernameError");
+const passwordError = document.querySelector("#passwordError");
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (userName.value.length === 0) {
+    usernameError.style.display = "block";
+    usernameError.innerHTML = "Input can't be empty";
+    userName.style.border = "2px solid red";
+  } else {
+    usernameError.style.display = "none";
+    userName.style.border = "1px solid rgb(186, 186, 186)";
+  }
+
+  if (password.value.length === 0) {
+    password.nextElementSibling.style.display = "block";
+    passwordError.innerHTML = "Input can't be empty";
+    password.style.border = "1px solid red";
+  } else if (password.value.length > 0 && password.value.length < 8) {
+    password.nextElementSibling.style.display = "block";
+    passwordError.innerHTML = "Password must have at least 8 charachters";
+    password.style.border = "1px solid red";
+  } else {
+    passwordError.style.display = "none";
+    password.style.border = "1px solid rgb(186, 186, 186)";
+  }
+});
+
